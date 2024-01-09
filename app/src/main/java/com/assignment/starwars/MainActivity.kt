3712458e22c6603d8d187e73a37f36000f7407fe
Pratyush Singh <aries.pratyush@gmail.com>
2024-01-09 18:48:44 +0530

    chore(deps): coroutines, hilt,retrofit dependencies

---
+++ app/src/main/java/com/assignment/starwars/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.assignment.starwars
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.assignment.starwars.databinding.ActivityMainBinding
+import com.assignment.starwars.ui.theme.StarWarsTheme
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/assignment/starwars/StarWarsApplication.kt
@@ -0,0 +1,8 @@
+package com.assignment.starwars
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StarWarsApplication: Application() {
+}
